0ff519b6b1bb2544bb37a9f4da2049c29ba5fa71
DY_Lee <dongyeoplee87@gmail.com>
2026-04-26 12:31:27 +0900

    Add Coding test roadmap and solve simple problem

---
+++ Coding_Test/Array_n_List/42586.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+int n = 100; // Array size
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    int MaxDay = 0;
+    vector<int> answer;
+
+    for (int i = 0; i < progresses.size(); i++) {
+        int current = (100 - progresses[i] + speeds[i] - 1) / speeds[i]; // 올림 공식
+
+        if (current > MaxDay) {
+            MaxDay = current;
+            answer.push_back(1);
+        } else {
+            answer.back()++;
+        }
+    }
+
+    return answer;
+}
